d27376883fd8d1464581fef1042c114dcc33b5b1
David Salamon <dls@lithp.org>
2020-09-10 21:59:41 -0700

    adds Kolmogorov-Smirnov value alongside the KL values

--- score-accuracy/kl_measure.jl
+++ score-accuracy/kl_measure.jl
@@ -8,7 +8,7 @@ test2_pos = get("test_true_pos.dat")
 test2_neg = get("test_false_pos.dat")
 
 function percent_greater_than(x, pos, neg)
-  pos_len = length(filter(e -> e > x, pos))
+  pos_len = length(filter(e -> e > x, pos)) + 1e-30 # prevent divide by 0
   neg_len = length(filter(e -> e > x, neg))
   return pos_len / (pos_len + neg_len)
 end
@@ -22,12 +22,23 @@ function kl(pos1, neg1, pos2, neg2)
       q = percent_greater_than(score, pos2[i], neg2[i]) + 1e-30 # prevent divide by 0
 
       kl = p * log(p/q)
-      if !isnan(kl)
-        sum += kl
-      end
+      sum += kl
     end
   end
   return sum
+end
+
+function ks(pos1, neg1, pos2, neg2)
+  ks = 0
+  for i in 1:10
+    for _score in 1:100
+      score = _score / 10
+      p = percent_greater_than(score, pos1[i], neg1[i])
+      q = percent_greater_than(score, pos2[i], neg2[i])
+      ks = max(abs(p - q), ks)
+    end
+  end
+  return ks
 end
 
 function main()
@@ -35,6 +46,8 @@ function main()
   @show dxy # -4.141558947348085
   dyx = kl(test2_pos, test2_neg, test1_pos, test1_neg)
   @show dyx # 4.315494163156585
+  ks_value = ks(test1_pos, test1_neg, test2_pos, test2_neg)
+  @show ks_value # 0.0378050482320722
 end
 
 main()
