5e60fdd3468b97419df82d99cec17b05ab52e8c6
David Salamon <dls@lithp.org>
2020-09-10 19:50:39 -0700

    clean up the code a bit

--- score-accuracy/kl_measure.jl
+++ score-accuracy/kl_measure.jl
@@ -1,24 +1,21 @@
 using Serialization
 
-get(file) = map(sort!, Serialization.deserialize(file))
+get(file) = Serialization.deserialize(file)
 
 test1_pos = get("true_pos.dat")
 test1_neg = get("false_pos.dat")
 test2_pos = get("test_true_pos.dat")
 test2_neg = get("test_false_pos.dat")
 
-function gt_pct(x, pos, neg)
+function percent_greater_than(x, pos, neg)
   pos_len = length(filter(e -> e > x, pos))
   neg_len = length(filter(e -> e > x, neg))
   return pos_len / (pos_len + neg_len)
 end
 
-sample_test1(x, i) = gt_pct(x, test1_pos[i], test1_neg[i])
-sample_test2(x, i) = gt_pct(x, test2_pos[i], test2_neg[i])
-
 function kl_i(x, i)
-  p = sample_test1(x, i)
-  q = sample_test2(x, i) + 1e-30
+  p = percent_greater_than(x, test1_pos[i], test1_neg[i])
+  q = percent_greater_than(x, test2_pos[i], test2_neg[i]) + 1e-30 # prevent divide by 0
   return p * log(p/q)
 end
 
