c5b72b5627de39b3b7557073d764f0a101d2fa59
David Salamon <dls@lithp.org>
2020-09-10 23:54:07 -0700

    adds alpha finding for a given ks value, finds them for our ks values

--- score-accuracy/kl_measure.jl
+++ score-accuracy/kl_measure.jl
@@ -29,7 +29,7 @@ function kl(pos1, neg1, pos2, neg2)
 end
 
 function ks(pos1, neg1, pos2, neg2)
-  ks_result = []
+  ks_result = Array{Float64,1}()
   for i in 1:10
     ks = 0
     for _score in 1:100
@@ -43,6 +43,21 @@ function ks(pos1, neg1, pos2, neg2)
   return ks_result
 end
 
+function seek_alpha(dnm, m, n)
+  c(a) = sqrt(-log(a/2) * 1/2)
+  cuttoff(a) = c(a) * sqrt((n + m) / (n * m))
+
+  a = 2
+  for step=2:15
+    step = 10.0^(-step)
+    while (a - step) > 0 && dnm > cuttoff(a - step)
+      a -= step
+    end
+  end
+
+  return a
+end
+
 function main()
   dxy = kl(test1_pos, test1_neg, test2_pos, test2_neg)
   @show dxy # -4.141558947348085
@@ -50,6 +65,22 @@ function main()
   @show dyx # 4.315494163156585
   ks_values = ks(test1_pos, test1_neg, test2_pos, test2_neg)
   @show ks_values # [0.006854177124148708, 0.0378050482320722, 0.014692124412983176, 0.012686104218362249, 0.018075259418633305, 0.021458405847546103, 0.018821473623260254, 0.015511943701060571, 0.01123827153453727, 0.01750482967210676]
+
+  alpha_values = Array{Float64, 1}()
+  for i=1:10
+    m = length(test1_pos[i]) + length(test1_neg[i])
+    n = length(test2_pos[i]) + length(test2_neg[i])
+    push!(alpha_values, seek_alpha(ks_values[i], m, n))
+  end
+  @show alpha_values # [1.9051527287531023, 0.3735660485695967, 1.6252196386751578, 1.6909527167668321, 1.4150531592839517, 1.343501539143379, 1.4099411445626444, 1.5558377724748356, 1.8014182269499501, 1.4703533523617984]
+
+  println()
+  # sanity checking: copy/paste to wolfram alpha
+  for i=1:10
+    m = length(test1_pos[i]) + length(test1_neg[i])
+    n = length(test2_pos[i]) + length(test2_neg[i])
+    println("sqrt(-log(a/2)/2) * sqrt($(n+m) / $(n*m)) = $(ks_values[i])")
+  end
 end
 
 main()
