73e4df9d0c2bffe88cabdb97f42c588e9309a873
David Salamon <dls@lithp.org>
2020-09-10 22:18:18 -0700

    fixes ks to operate on each category individually

--- score-accuracy/kl_measure.jl
+++ score-accuracy/kl_measure.jl
@@ -29,16 +29,18 @@ function kl(pos1, neg1, pos2, neg2)
 end
 
 function ks(pos1, neg1, pos2, neg2)
-  ks = 0
+  ks_result = []
   for i in 1:10
+    ks = 0
     for _score in 1:100
       score = _score / 10
       p = percent_greater_than(score, pos1[i], neg1[i])
       q = percent_greater_than(score, pos2[i], neg2[i])
       ks = max(abs(p - q), ks)
     end
+    push!(ks_result, ks)
   end
-  return ks
+  return ks_result
 end
 
 function main()
@@ -46,8 +48,8 @@ function main()
   @show dxy # -4.141558947348085
   dyx = kl(test2_pos, test2_neg, test1_pos, test1_neg)
   @show dyx # 4.315494163156585
-  ks_value = ks(test1_pos, test1_neg, test2_pos, test2_neg)
-  @show ks_value # 0.0378050482320722
+  ks_values = ks(test1_pos, test1_neg, test2_pos, test2_neg)
+  @show ks_values # [0.006854177124148708, 0.0378050482320722, 0.014692124412983176, 0.012686104218362249, 0.018075259418633305, 0.021458405847546103, 0.018821473623260254, 0.015511943701060571, 0.01123827153453727, 0.01750482967210676]
 end
 
 main()
